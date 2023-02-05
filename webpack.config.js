3564106e9ab9b3aeb431bab633daa3d7d1058628
akashpinnani <varmaakash95@gmail.com>
2023-02-04 21:17:07 -0600

    add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js'),
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack Starter",
-            filename: "index.html",
-            template: "src/template.html"
-        })
-    ]
-}
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Starter",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
