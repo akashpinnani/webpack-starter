f6052d7e33b8d41c54bb07fedddb8e205da4a420
akashpinnani <varmaakash95@gmail.com>
2023-02-04 23:23:06 -0600

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+
   entry: {
     bundle: path.resolve(__dirname, "src/index.js"),
   },
@@ -11,7 +12,10 @@ module.exports = {
     filename: "[name][contenthash].js",
     clean: true,
   },
-  devtool: 'source-map',
+  //source maps enabled
+  devtool: "source-map",
+
+  //server configuration
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
@@ -22,14 +26,32 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
   },
+
+  //loaders
   module: {
     rules: [
+        //scss loader
       {
+        //check if any scss files are present and use below loaders if there is a match
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+
+      //babel loader - used for backward compatability for older js to support in all browsers
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            }
+        }
+      }
     ],
   },
+
+  //plugins
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack Starter",
